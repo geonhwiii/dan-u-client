8c3b5f437bdb7274425059774b360fcb51455bbf
geonhwiii <geonhwiii@github.com>
2022-12-10 14:12:43 +0900

    add default setting & font pretendard

---
+++ utils/local-font/index.ts
@@ -0,0 +1,13 @@
+import NextFont from "@next/font/local";
+
+export const localFont = NextFont({
+  src: [
+    { path: "../../assets/fonts/PretendardStd-Bold.woff2", weight: "bold" },
+    {
+      path: "../../assets/fonts/PretendardStd-Regular.woff2",
+      weight: "normal",
+    },
+  ],
+  variable: "--font-pretendard",
+  preload: true,
+});
